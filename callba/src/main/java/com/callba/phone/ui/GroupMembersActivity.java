c6f3fb58dfe21ffcc1605c6d77f79c4c490ab12f
chenlingzhi39 <chenlingzhi39@gmail.com>
2016-10-27 00:38:41 +0800

    fix

--- callba/src/main/java/com/callba/phone/ui/GroupMembersActivity.java
+++ callba/src/main/java/com/callba/phone/ui/GroupMembersActivity.java
@@ -2,38 +2,53 @@ package com.callba.phone.ui;
 
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.Filter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.callba.R;
 import com.callba.phone.annotation.ActivityFragmentInject;
-import com.callba.phone.bean.ContactMultiNumBean;
 import com.callba.phone.bean.EaseUser;
+import com.callba.phone.bean.GroupMember;
 import com.callba.phone.bean.Member;
 import com.callba.phone.pinyin.CharacterParser;
 import com.callba.phone.ui.adapter.GroupMemberAdapter;
 import com.callba.phone.ui.adapter.RecyclerArrayAdapter;
 import com.callba.phone.ui.adapter.expandRecyclerviewadapter.StickyRecyclerHeadersDecoration;
 import com.callba.phone.ui.base.BaseActivity;
-import com.callba.phone.util.ContactsAccessPublic;
 import com.callba.phone.util.EaseUserUtils;
+import com.callba.phone.util.InitiateSearch;
 import com.callba.phone.util.Logger;
 import com.callba.phone.widget.DividerDecoration;
 import com.callba.phone.widget.SideBar;
 import com.hyphenate.EMGroupChangeListener;
 import com.hyphenate.chat.EMClient;
+import com.hyphenate.chat.EMConversation;
 import com.hyphenate.exceptions.HyphenateException;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -44,7 +59,8 @@ import butterknife.ButterKnife;
 @ActivityFragmentInject(
         contentViewId = R.layout.activity_group_member,
         toolbarTitle = R.string.group_member,
-        navigationId = R.drawable.press_back
+        navigationId = R.drawable.press_back,
+        menuId = R.menu.message
 )
 public class GroupMembersActivity extends BaseActivity {
 
@@ -56,10 +72,20 @@ public class GroupMembersActivity extends BaseActivity {
     TextView dialog;
     @BindView(R.id.refresh_layout)
     SwipeRefreshLayout refreshLayout;
+    @BindView(R.id.image_search_back)
+    ImageView imageSearchBack;
+    @BindView(R.id.edit_text_search)
+    EditText editTextSearch;
+    @BindView(R.id.clearSearch)
+    ImageView clearSearch;
+    @BindView(R.id.linearLayout_search)
+    LinearLayout linearLayoutSearch;
+    @BindView(R.id.card_search)
+    CardView cardSearch;
     private GroupMemberAdapter groupMemberAdapter;
     private ArrayList<Member> members;
     private EMGroupChangeListener emGroupChangeListener;
-
+    private Filter filter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -130,16 +156,16 @@ public class GroupMembersActivity extends BaseActivity {
         groupMemberAdapter.setOnItemClickListener(new RecyclerArrayAdapter.OnItemClickListener() {
             @Override
             public void onItemClick(int position) {
-                startActivityForResult(new Intent(GroupMembersActivity.this, GroupUserInfoActivity.class).putExtra("username", groupMemberAdapter.getData().get(position).getName()).putExtra("group_id",getIntent().getStringExtra("group_id")),0);
+                startActivityForResult(new Intent(GroupMembersActivity.this, GroupUserInfoActivity.class).putExtra("username", groupMemberAdapter.getData().get(position).getName()).putExtra("group_id", getIntent().getStringExtra("group_id")), 0);
             }
         });
         groupMemberAdapter.setOnItemLongClickListener(new RecyclerArrayAdapter.OnItemLongClickListener() {
             @Override
             public boolean onItemClick(int position) {
-              if(getIntent().getStringExtra("group_owner").equals(EMClient.getInstance().getCurrentUser())){
-                  if(!groupMemberAdapter.getItem(position).getName().equals(EMClient.getInstance().getCurrentUser()))
-                      showDeleteDialog(groupMemberAdapter.getItem(position));
-              }
+                if (getIntent().getStringExtra("group_owner").equals(EMClient.getInstance().getCurrentUser())) {
+                    if (!groupMemberAdapter.getItem(position).getName().equals(EMClient.getInstance().getCurrentUser()))
+                        showDeleteDialog(groupMemberAdapter.getItem(position));
+                }
                 return false;
             }
         });
@@ -162,6 +188,8 @@ public class GroupMembersActivity extends BaseActivity {
                 headersDecor.invalidateHeaders();
             }
         });
+        InitiateSearch();
+        HandleSearch();
         refresh();
     }
 
@@ -198,19 +226,65 @@ public class GroupMembersActivity extends BaseActivity {
                 }
             }
         });
-        groupMemberAdapter.clear();
-        groupMemberAdapter.addAll(members);
+        filter=new MyFilter(members);
+        if(TextUtils.isEmpty(editTextSearch.getText().toString()))
+        {groupMemberAdapter.clear();
+        groupMemberAdapter.addAll(members);}else
+            filter.filter(editTextSearch.getText().toString());
+
+    }
+    private void InitiateSearch() {
+        editTextSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (filter != null)
+                    filter.filter(s);
+                if (editTextSearch.getText().toString().length() == 0) {
+                    clearSearch.setVisibility(View.GONE);
+                } else {
+                    clearSearch.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        clearSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editTextSearch.setText("");
+                ((InputMethodManager) GroupMembersActivity.this.getSystemService(Context.INPUT_METHOD_SERVICE)).toggleSoftInput(InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY);
+            }
+        });
 
     }
 
+    private void HandleSearch() {
+        imageSearchBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("search", "back");
+                InitiateSearch.handleToolBar(GroupMembersActivity.this, cardSearch, editTextSearch, 20);
+            }
+        });
+        editTextSearch.requestFocus();
+    }
     @Override
     protected void onDestroy() {
         super.onDestroy();
         EMClient.getInstance().groupManager().removeGroupChangeListener(emGroupChangeListener);
     }
+
     private void showDeleteDialog(final Member member) {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setItems(new String[]{getString(R.string.remove)+"\""+ EaseUserUtils.getUserNick(member.getName())+"\"",getString(R.string.move_to_black_list)},
+        builder.setItems(new String[]{getString(R.string.remove) + "\"" + EaseUserUtils.getUserNick(member.getName()) + "\"", getString(R.string.move_to_black_list)},
                 new DialogInterface.OnClickListener() {
 
                     public void onClick(DialogInterface dialog, int which) {
@@ -229,6 +303,7 @@ public class GroupMembersActivity extends BaseActivity {
                 });
         builder.create().show();
     }
+
     /**
      * 删除群成员
      *
@@ -251,6 +326,8 @@ public class GroupMembersActivity extends BaseActivity {
                         @Override
                         public void run() {
                             groupMemberAdapter.remove(member);
+                            members.remove(member);
+                            filter=new MyFilter(members);
                             setResult(RESULT_OK);
                             deleteDialog.dismiss();
                         }
@@ -281,6 +358,8 @@ public class GroupMembersActivity extends BaseActivity {
                         public void run() {
                             pd.dismiss();
                             groupMemberAdapter.remove(member);
+                            members.remove(member);
+                            filter=new MyFilter(members);
                             setResult(RESULT_OK);
                             Toast.makeText(getApplicationContext(), R.string.Move_into_blacklist_success, Toast.LENGTH_SHORT).show();
                         }
@@ -296,10 +375,80 @@ public class GroupMembersActivity extends BaseActivity {
             }
         }).start();
     }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-       if(resultCode==RESULT_OK)
-           groupMemberAdapter.notifyDataSetChanged();
+        if (resultCode == RESULT_OK)
+            groupMemberAdapter.notifyDataSetChanged();
     }
-}
+    public class MyFilter extends Filter {
+        List<Member> mOriginalList;
+
+        public MyFilter(List<Member> messages) {
+            this.mOriginalList = messages;
+        }
+
+        @Override
+        protected FilterResults performFiltering(CharSequence prefix) {
+            FilterResults results = new FilterResults();
+            if (mOriginalList == null) {
+                mOriginalList = new ArrayList<>();
+            }
+
+
+            if (prefix == null || prefix.length() == 0) {
+                results.values = mOriginalList;
+                results.count = mOriginalList.size();
+            } else {
+                String prefixString = prefix.toString();
+                final int count = mOriginalList.size();
+                final ArrayList<Member> newValues = new ArrayList<>();
+                for (int i = 0; i < count; i++) {
+                    final Member member = mOriginalList.get(i);
+//                    String username = user.getNick();
+//                    if(username == null)
+//                        username = user.getNick();
+                    String username = member.getName();
+
+                        EaseUser user = EaseUserUtils.getUserInfo(member.getName());
+                        if (user != null) {
+                            if (!TextUtils.isEmpty(user.getNick()))
+                                username = user.getNick();
+                            if (!TextUtils.isEmpty(user.getRemark()))
+                                username = user.getRemark();
+                        }
+                        if (username.contains(prefixString)) {
+                            newValues.add(member);
+                        }
+                    }
+                    results.values = newValues;
+                    results.count = newValues.size();
+                }
+                return results;
+            }
+
+            @Override
+            protected void publishResults (CharSequence constraint, FilterResults results){
+                groupMemberAdapter.clear();
+                groupMemberAdapter.addAll((ArrayList<Member>) results.values);
+            }
+        }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.search:
+                InitiateSearch.handleToolBar(GroupMembersActivity.this, cardSearch, editTextSearch, 20);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        ((InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE)).hideSoftInputFromWindow(cardSearch.getWindowToken(), 0);
+        if(cardSearch.getVisibility()== View.VISIBLE&&TextUtils.isEmpty(editTextSearch.getText().toString()))
+            InitiateSearch.handleToolBar(GroupMembersActivity.this, cardSearch, editTextSearch, 20);
+    }
+    }
+
